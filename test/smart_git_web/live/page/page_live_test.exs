a8ea9e779b4864221351f90d4c0fd2a08c03d6cc
davimonteiro7 <davi.monteiro7@gmail.com>
2022-04-14 19:13:42 -0300

    feat: add first components

--- test/smart_git_web/live/page/page_live_test.exs
+++ test/smart_git_web/live/page/page_live_test.exs
@@ -6,5 +6,9 @@ defmodule SmartGitWeb.PageLiveTest do
     {:ok, view, _html} = live(conn, Routes.page_path(conn, :index))
 
     assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Elixir")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=rust]", "Rust")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=javascript]", "Javascript")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=java]", "Java")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=haskell]", "Haskell")
   end
 end
