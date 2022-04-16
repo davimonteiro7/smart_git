8965ff08b586fc364ccd065a05c3c97edf03d7bc
davimonteiro7 <davi.monteiro7@gmail.com>
2022-04-16 20:00:38 -0300

    insert into database

---
+++ lib/smart_git/git_repos.ex
@@ -0,0 +1,9 @@
+defmodule SmartGit.GitRepos do
+  alias SmartGit.GitRepos.GitRepo
+  alias SmartGit.Repo
+  def create(repo) do
+    %GitRepo{}
+    |> GitRepo.changeset(repo)
+    |> Repo.insert
+  end
+end
